c26d8b12cd2366c29d4c01d729a3621360aba74d
anandarizkyrm <rizky.pky@gmail.com>
2023-01-11 19:22:44 +0700

    sdsd

--- src/components/molecules/CardTodo/CardTodo.tsx
+++ src/components/molecules/CardTodo/CardTodo.tsx
@@ -25,17 +25,25 @@ const CardTodo = (props: Props) => {
 
   const navigate = useNavigate();
   return (
-    <div data-cy="activity-item" className="card-todo">
-      <h3 data-cy="activity-item-title" onClick={() => navigate(`/detail/${id}`)}>
-        {title}
-      </h3>
-      <div className="card-bottom">
-        <h4 data-cy="activity-item-date" className="card-todo-date">
-          {dayjs(date).locale('id').format('DD MMMM YYYY')}
-        </h4>
-        <div data-cy="activity-item-delete-button" onClick={handleButtonDeleteOnClick}>
-          <img alt="delete-icon" src={deleteIcon}></img>
+    <div style={{ position: 'relative' }} className="">
+      <div
+        onClick={() => navigate(`/detail/${id}`)}
+        data-cy="activity-item"
+        className="card-todo"
+      >
+        <h3 data-cy="activity-item-title">{title}</h3>
+        <div className="card-bottom">
+          <h4 data-cy="activity-item-date" className="card-todo-date">
+            {dayjs(date).locale('id').format('DD MMMM YYYY')}
+          </h4>
         </div>
+      </div>
+      <div
+        style={{ position: 'absolute', bottom: 46, right: 32 }}
+        data-cy="activity-item-delete-button"
+        onClick={handleButtonDeleteOnClick}
+      >
+        <img alt="delete-icon" src={deleteIcon}></img>
       </div>
     </div>
   );
